39b5bfef4d0212fc1949f1acc92e317c7a51f60b
ashutoshbhatt1 <35776394+ashutoshbhatt1@users.noreply.github.com>
2020-03-10 12:05:46 -0500

    Update run-iperf.sh

--- run-iperf.sh
+++ run-iperf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in *.TDG.kubeconfig; do mv $i `echo $i | tr [:upper:] [:lower:]`; done
-export KUBECONFIG=$HOSTNAME.TDG.kubeconfig
+export KUBECONFIG=$HOSTNAME.tdg.kubeconfig
 set -eu
 
 cd $(dirname $0)
